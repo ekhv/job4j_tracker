1b7e840f4fa2629626b05a1c98d537ac001728ad
Evgeniy Khryashchev <e.khryashchev@outlook.com>
2021-08-30 22:04:03 +0500

    3. Arrays and models

---
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -0,0 +1,31 @@
+package ru.job4j.pojo;
+
+public class Library {
+    public static void main(String[] args) {
+        Book cleanCode = new Book("Clean Code", 1137);
+        Book headFirst = new Book("Head First Design Patterns", 954);
+        Book javaPerformance = new Book("Java Performance", 543);
+        Book releaseIt = new Book("Release It!", 426);
+        Book[] books = new Book[4];
+        books[0] = cleanCode;
+        books[1] = headFirst;
+        books[2] = javaPerformance;
+        books[3] = releaseIt;
+        for (int i = 0; i < books.length; i++) {
+            System.out.println(books[i].getName());
+        }
+        System.out.println();
+        Book temp = books[0];
+        books[0] = books[3];
+        books[3] = temp;
+        for (int i = 0; i < books.length; i++) {
+            System.out.println(books[i].getName());
+        }
+        System.out.println();
+        for (int i = 0; i < books.length; i++) {
+            if (books[i].getName().equals("Clean Code")) {
+                System.out.println(books[i].getName());
+            }
+        }
+    }
+}
