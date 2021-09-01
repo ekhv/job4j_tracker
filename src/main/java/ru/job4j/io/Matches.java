6246755352473da3ba9ea4921e4a6015b202269b
Evgeniy Khryashchev <e.khryashchev@outlook.com>
2021-09-01 23:05:09 +0500

    2. Scanner and reading a number from the console

---
+++ src/main/java/ru/job4j/io/Matches.java
@@ -0,0 +1,25 @@
+package ru.job4j.io;
+
+import java.util.Scanner;
+
+public class Matches {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Игра 11.");
+        boolean turn = true;
+        int count = 11;
+        do {
+            String player = turn ? "Первый игрок" : "Второй игрок";
+            System.out.println(player + " введите число от 1 до 3:");
+            int matches = Integer.parseInt(input.nextLine());
+            count -= matches;
+            System.out.println("Осталось : " + count);
+            turn = !turn;
+        } while (count > 0);
+        if (!turn) {
+            System.out.println("Выиграл первый игрок");
+        } else {
+            System.out.println("Выиграл второй игрок");
+        }
+    }
+}
