23333281d54b1af3f73bb491437470a56b1b3108
Evgeniy Khryashchev <e.khryashchev@outlook.com>
2021-09-02 21:59:01 +0500

    return value check

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -47,9 +47,13 @@ public class Tracker {
 
     public boolean replace(int id, Item item) {
         int index = indexOf(id);
-        item.setId(id);
-        items[index] = item;
-        return index != -1;
+        boolean rsl = index != -1;
+        if (rsl) {
+            item.setId(id);
+            items[index] = item;
+            return true;
+        }
+        return false;
     }
 
     public boolean delete(int id) {
