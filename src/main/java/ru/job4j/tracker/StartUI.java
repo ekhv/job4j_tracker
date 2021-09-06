398d3ab1df2c7b7ebcef23114327ffab6cf23a3b
Evgeniy Khryashchev <e.khryashchev@outlook.com>
2021-09-06 22:48:33 +0500

    9.1 Dependency on System.out

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,6 +1,28 @@
 package ru.job4j.tracker;
 
 public class StartUI {
+    private final Output out;
+
+    public StartUI(Output out) {
+        this.out = out;
+    }
+
+    public static void main(String[] args) {
+        Output output = new ConsoleOutput();
+        Input input = new ConsoleInput();
+        Tracker tracker = new Tracker();
+        UserAction[] actions = {
+                new CreateAction(output),
+                new FindAll(output),
+                new ReplaceAction(output),
+                new DeleteAction(output),
+                new FindById(output),
+                new FindByName(output),
+                new ExitAction(output)
+        };
+        new StartUI(output).init(input, tracker, actions);
+    }
+
     public void init(Input input, Tracker tracker, UserAction[] actions) {
         boolean run = true;
         while (run) {
@@ -12,24 +34,9 @@ public class StartUI {
     }
 
     private void showMenu(UserAction[] actions) {
-        System.out.println("Menu.");
+        out.println("Menu.");
         for (int index = 0; index < actions.length; index++) {
-            System.out.println(index + ". " + actions[index].name());
+            out.println(index + ". " + actions[index].name());
         }
-    }
-
-    public static void main(String[] args) {
-        Input input = new ConsoleInput();
-        Tracker tracker = new Tracker();
-        UserAction[] actions = {
-                new CreateAction(),
-                new FindAll(),
-                new ReplaceAction(),
-                new DeleteAction(),
-                new FindById(),
-                new FindByName(),
-                new ExitAction()
-        };
-        new StartUI().init(input, tracker, actions);
     }
 }
--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -7,47 +7,85 @@ import static org.junit.Assert.assertThat;
 
 public class StartUITest {
     @Test
+    public void whenExit() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[]{"0"}
+        );
+        Tracker tracker = new Tracker();
+        UserAction[] actions = {
+                new ExitAction(out)
+        };
+        new StartUI(out).init(in, tracker, actions);
+        assertThat(out.toString(), is(
+                "Menu." + System.lineSeparator() +
+                        "0. Exit Program" + System.lineSeparator() + "Выход" + System.lineSeparator()
+        ));
+    }
+
+    @Test
     public void whenReplaceItem() {
+        Output out = new StubOutput();
         Input in = new StubInput(
                 new String[]{"0", "Item name", "1", "1", "replaced item", "2"}
         );
         Tracker tracker = new Tracker();
         UserAction[] actions = {
-                new CreateAction(),
-                new ReplaceAction(),
-                new ExitAction()
+                new CreateAction(out),
+                new ReplaceAction(out),
+                new ExitAction(out)
         };
-        new StartUI().init(in, tracker, actions);
+        new StartUI(out).init(in, tracker, actions);
         Item replaced = tracker.findAll()[0];
         assertThat(replaced.getName(), is("replaced item"));
     }
 
     @Test
     public void whenDeleteItem() {
+        Output out = new StubOutput();
         Input in = new StubInput(
-                new String[]{"0", "Item name", "1", "1", "2"}
+                new String[]{"0", "1", "1"}
         );
         Tracker tracker = new Tracker();
         UserAction[] actions = {
-                new CreateAction(),
-                new DeleteAction(),
-                new ExitAction()
+                new DeleteAction(out),
+                new ExitAction(out)
         };
-        new StartUI().init(in, tracker, actions);
-        assert tracker.findAll().length == 0;
+        new StartUI(out).init(in, tracker, actions);
+        assertThat(out.toString(), is(
+                "Menu."
+                        + System.lineSeparator()
+                        + "0. Delete Item"
+                        + System.lineSeparator()
+                        + "1. Exit Program"
+                        + System.lineSeparator()
+                        + "=== Delete item ===="
+                        + System.lineSeparator()
+                        + "Ошибка удаления заявки."
+                        + System.lineSeparator()
+                        + "Menu."
+                        + System.lineSeparator()
+                        + "0. Delete Item"
+                        + System.lineSeparator()
+                        + "1. Exit Program"
+                        + System.lineSeparator()
+                        + "Выход"
+                        + System.lineSeparator()
+        ));
     }
 
     @Test
     public void whenCreateItem() {
+        Output out = new StubOutput();
         Input in = new StubInput(
                 new String[]{"0", "Item name", "1"}
         );
         Tracker tracker = new Tracker();
         UserAction[] actions = {
-                new CreateAction(),
-                new ExitAction()
+                new CreateAction(out),
+                new ExitAction(out)
         };
-        new StartUI().init(in, tracker, actions);
+        new StartUI(out).init(in, tracker, actions);
         assertThat(tracker.findAll()[0].getName(), is("Item name"));
     }
 }
