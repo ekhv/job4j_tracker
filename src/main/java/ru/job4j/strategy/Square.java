70489d148054557ffab04907a7fb9ad7564b994d
Evgeniy Khryashchev <e.khryashchev@outlook.com>
2021-09-06 21:34:58 +0500

    one entry point

--- src/main/java/ru/job4j/strategy/Square.java
+++ src/main/java/ru/job4j/strategy/Square.java
@@ -9,10 +9,6 @@ public class Square implements Shape {
                 + "*      *" + ln
                 + "********";
     }
-
-    public static void main(String[] args) {
-        System.out.println(new Triangle().draw());
-    }
 }
 
 
