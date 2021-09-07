204f70319e6dc32a7204bcb3f00be9b91c6cb13d
Evgeniy Khryashchev <e.khryashchev@outlook.com>
2021-09-07 14:43:04 +0500

    9.2. Console Output Tests

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -88,4 +88,106 @@ public class StartUITest {
         new StartUI(out).init(in, tracker, actions);
         assertThat(tracker.findAll()[0].getName(), is("Item name"));
     }
+
+    @Test
+    public void whenFindAllAction(){
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[]{"0", "1"}
+        );
+        Tracker tracker = new Tracker();
+        UserAction[] actions = {
+                new FindAll(out),
+                new ExitAction(out)
+        };
+        new StartUI(out).init(in, tracker, actions);
+        assertThat(out.toString(), is(
+                "Menu."
+                        + System.lineSeparator()
+                        + "0. Show all items"
+                        + System.lineSeparator()
+                        + "1. Exit Program"
+                        + System.lineSeparator()
+                        + "=== Show all items ===="
+                        + System.lineSeparator()
+                        + "Хранилище еще не содержит заявок"
+                        + System.lineSeparator()
+                        + "Menu."
+                        + System.lineSeparator()
+                        + "0. Show all items"
+                        + System.lineSeparator()
+                        + "1. Exit Program"
+                        + System.lineSeparator()
+                        + "Выход"
+                        + System.lineSeparator()
+        ));
+    }
+
+    @Test
+    public void whenFindByNameAction(){
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[]{"0","Item name", "1"}
+        );
+        Tracker tracker = new Tracker();
+        UserAction[] actions = {
+                new FindByName(out),
+                new ExitAction(out)
+        };
+        new StartUI(out).init(in, tracker, actions);
+        assertThat(out.toString(), is(
+                "Menu."
+                        + System.lineSeparator()
+                        + "0. Find item by name"
+                        + System.lineSeparator()
+                        + "1. Exit Program"
+                        + System.lineSeparator()
+                        + "=== Find items by name ===="
+                        + System.lineSeparator()
+                        + "Заявки с именем: Item name не найдены."
+                        + System.lineSeparator()
+                        + "Menu."
+                        + System.lineSeparator()
+                        + "0. Find item by name"
+                        + System.lineSeparator()
+                        + "1. Exit Program"
+                        + System.lineSeparator()
+                        + "Выход"
+                        + System.lineSeparator()
+        ));
+    }
+
+    @Test
+    public void whenFindByIdAction(){
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[]{"0", "1", "1"}
+        );
+        Tracker tracker = new Tracker();
+        UserAction[] actions = {
+                new FindById(out),
+                new ExitAction(out)
+        };
+        new StartUI(out).init(in, tracker, actions);
+        assertThat(out.toString(), is(
+                "Menu."
+                        + System.lineSeparator()
+                        + "0. Find item by id"
+                        + System.lineSeparator()
+                        + "1. Exit Program"
+                        + System.lineSeparator()
+                        + "=== Find item by id ===="
+                        + System.lineSeparator()
+                        + "Заявка с введенным id: 1 не найдена."
+                        + System.lineSeparator()
+                        + "Menu."
+                        + System.lineSeparator()
+                        + "0. Find item by id"
+                        + System.lineSeparator()
+                        + "1. Exit Program"
+                        + System.lineSeparator()
+                        + "Выход"
+                        + System.lineSeparator()
+        ));
+    }
 }
