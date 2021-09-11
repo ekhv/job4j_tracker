0518707782afc92ab1e8ecbc78045052f8a25d6d
Evgeniy Khryashchev <e.khryashchev@outlook.com>
2021-09-11 17:18:18 +0500

    fixed test with substitution of different positive numbers

--- src/test/java/ru/job4j/tracker/ValidateInputTest.java
+++ src/test/java/ru/job4j/tracker/ValidateInputTest.java
@@ -20,15 +20,16 @@ public class ValidateInputTest {
     @Test
     public void whenInvalidInput123Then123() {
         Output out = new StubOutput();
-        String[] values = new String[]{"1", "2", "3"};
-        for (String val: values) {
-            Input in = new StubInput(
-                    new String[]{val}
-            );
-            ValidateInput input = new ValidateInput(out, in);
-            int selected = input.askInt("Enter menu:");
-            assertThat(selected, is(Integer.parseInt(val)));
-        }
+        Input in = new StubInput(
+                new String[]{"1", "2", "3"}
+        );
+        ValidateInput input = new ValidateInput(out, in);
+        int selectedFirst = input.askInt("Enter menu:");
+        int selectedSecond = input.askInt("Enter menu:");
+        int selectedThird = input.askInt("Enter menu:");
+        assertThat(selectedFirst, is(1));
+        assertThat(selectedSecond, is(2));
+        assertThat(selectedThird, is(3));
     }
 
     @Test
