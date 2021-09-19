78667481f6f2724e065d4e4dc98b3bd9c321968b
Evgeniy Khryashchev <e.khryashchev@outlook.com>
2021-09-19 22:51:00 +0500

    search for a value across all fields

--- src/main/java/ru/job4j/search/PhoneDictionary.java
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -20,7 +20,8 @@ public class PhoneDictionary {
         for (Person person : persons) {
             if (person.getName().contains(key)
                     || person.getSurname().contains(key)
-                    || person.getPhone().contains(key)) {
+                    || person.getPhone().contains(key)
+                    || person.getAddress().contains(key)) {
                 result.add(person);
             }
         }
