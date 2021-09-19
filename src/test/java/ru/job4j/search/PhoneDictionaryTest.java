5236884447e0fc39fa1eb92212862bd4766d3258
Evgeniy Khryashchev <e.khryashchev@outlook.com>
2021-09-19 22:50:10 +0500

    test when ArrayList is empty

--- src/test/java/ru/job4j/search/PhoneDictionaryTest.java
+++ src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -3,8 +3,9 @@ package ru.job4j.search;
 import org.junit.Test;
 
 import java.util.ArrayList;
+
 import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.*;
 
 public class PhoneDictionaryTest {
     @Test
@@ -15,5 +16,15 @@ public class PhoneDictionaryTest {
         );
         ArrayList<Person> persons = phones.find("Petr");
         assertThat(persons.get(0).getSurname(), is("Arsentev"));
+    }
+
+    @Test
+    public void whenFindByNameEmptyArrayList() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        ArrayList<Person> persons = phones.find("Vladimir");
+        assertEquals(0, persons.size());
     }
 }
