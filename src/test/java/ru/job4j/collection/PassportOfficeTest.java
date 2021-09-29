6dc8315bcef140577b150dd0e835e433071b81ef
Evgeniy Khryashchev <e.khryashchev@outlook.com>
2021-09-29 23:04:53 +0500

    check for adding a duplicate to HashMap

--- src/test/java/ru/job4j/collection/PassportOfficeTest.java
+++ src/test/java/ru/job4j/collection/PassportOfficeTest.java
@@ -14,4 +14,14 @@ public class PassportOfficeTest {
         office.add(citizen);
         assertThat(office.get(citizen.getPassport()), is(citizen));
     }
+
+    @Test
+    public void addDouble() {
+        Citizen citizenFirst = new Citizen("2f44a", "Petr Arsentev");
+        Citizen citizenSecond = new Citizen("2f44a", "Petr Arsentev");
+        PassportOffice office = new PassportOffice();
+        office.add(citizenFirst);
+        office.add(citizenSecond);
+        assertThat(office.add(citizenSecond), is(false));
+    }
 }
